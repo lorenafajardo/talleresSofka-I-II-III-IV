0078249e8ac76b3d7ae3c294c479fc23db012928
Lorena Fajardo Díaz <87463011+lorenafajardo@users.noreply.github.com>
2022-02-28 22:02:43 -0500

    Rename EjercicioVII.java to EjercicioVIII.java

--- Taller3/src/Entidades/EjercicioVIII.java
+++ Taller3/src/Entidades/EjercicioVIII.java
@@ -1,11 +1,11 @@
 
-//8.	La escuela automovilÌstica "El Maestro" requiere una aplicaciÛn que permita registrar 
-// a sus clientes en los cursos de enseÒanza automovilÌstica y establecer quienes lo han 
-// aprobado para continuar con el tr·mite de adquirir la licencia de conducciÛn. Para cada 
+//8.	La escuela automovil√≠stica "El Maestro" requiere una aplicaci√≥n que permita registrar 
+// a sus clientes en los cursos de ense√±anza automovil√≠stica y establecer quienes lo han 
+// aprobado para continuar con el tr√°mite de adquirir la licencia de conducci√≥n. Para cada 
 // usuario se debe permitir registrar su ingreso al curso, consultar usuarios que hayan 
 // presentado el curso y resultados de la prueba del curso (si fueron aprobados o no). 
-// Recuerde que el sistema debe terminar cuando el usuario asÌ lo indique. 
-// Tenga presente que la escuela tiene capacidad m·xima de gestionar 8 usuarios en su totalidad.
+// Recuerde que el sistema debe terminar cuando el usuario as√≠ lo indique. 
+// Tenga presente que la escuela tiene capacidad m√°xima de gestionar 8 usuarios en su totalidad.
 
 package Entidades;
 
